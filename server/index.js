fd770e1d6ec745f2747f71a6be45e9e50059b382
zhak1230 <zhak1230@gmail.com>
2022-05-04 22:54:37 +0900

    MongoDB 연결

--- server/index.js
+++ server/index.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
 const app = express();
 const port = 5000;
 
+// mongodb+srv://zhak1230:dnjsdud12@@cluster0.qo0ws.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
 app.use(express.static(path.join(__dirname, '../client/build')));
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  mongoose
+    .connect(
+      'mongodb+srv://zhak1230:dnjsdud639@cluster0.qo0ws.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    )
+    .then(() => {
+      console.log(`Example app listening at http://localhost:${port}`);
+      console.log('Connecting MongoDB...');
+    })
+    .catch((err) => {
+      console.log(`${err}`);
+    });
 });
 
 app.get('/', (요청, 응답) => {
+  응답.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+app.get('*', (요청, 응답) => {
   응답.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
