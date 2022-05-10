71c0c60c1255acb253d267ca0782a7452f01aeca
zhak1230 <zhak1230@gmail.com>
2022-05-10 18:53:33 +0900

    User 폴더 생성 및 Test

---
+++ client/src/Component/User/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div>
+      <input type='email' />
+      <input type='password' />
+      <button>로그인</button>
+      <button>회원가입</button>
+    </div>
+  );
+}
+
+export default Login;
---
+++ client/src/Component/User/Register.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Register() {
+  return (
+    <div>
+      <input type='name' />
+      <input type='email' />
+      <input type='password' />
+      <input type='password' />
+      <button>회원가입</button>
+    </div>
+  );
+}
+
+export default Register;
