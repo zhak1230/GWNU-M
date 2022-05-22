396ead53cbdae3b267e5431dcde18aa074669b94
zhak1230 <zhak1230@gmail.com>
2022-05-22 13:55:20 +0900

    Reple Update : 댓글 수정하기

--- client/src/Component/Reple/RepleContent.js
+++ client/src/Component/Reple/RepleContent.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
 import { useSelector } from 'react-redux';
 
 import { RepleContentDiv, RepleUploadDiv } from '../../Style/RepleCSS.js';
@@ -6,14 +7,31 @@ import { RepleContentDiv, RepleUploadDiv } from '../../Style/RepleCSS.js';
 function RepleContent(props) {
   const [ModalFlag, setModalFlag] = useState(false);
   const [Editflag, setEditflag] = useState(false);
-  const [Reple, setReple] = useState('');
+  const [Reple, setReple] = useState(props.reple.reple);
 
   const user = useSelector((state) => state.user);
 
   const ref = useRef();
   useOnClickOutside(ref, () => setModalFlag(false));
 
-  const SubmitHandler = () => {};
+  const SubmitHandler = (e) => {
+    e.preventDefault();
+    let body = {
+      uid: user.uid,
+      reple: Reple,
+      postId: props.reple.postId,
+      repleId: props.reple._id,
+    };
+
+    axios.post('/api/reple/edit', body).then((response) => {
+      if (response.data.success) {
+        alert('댓글 수정을 완료하였습니다.');
+      } else {
+        alert('댓글 수정에 실패하였습니다.');
+      }
+      return window.location.reload();
+    });
+  };
 
   return (
     <RepleContentDiv>
@@ -34,6 +52,7 @@ function RepleContent(props) {
                 <p
                   onClick={() => {
                     setEditflag(true);
+                    setModalFlag(false);
                   }}
                 >
                   수정
@@ -62,6 +81,16 @@ function RepleContent(props) {
               등록
             </button>
           </form>
+          <div className='cancel'>
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                setEditflag(false);
+              }}
+            >
+              취소
+            </button>
+          </div>
         </RepleUploadDiv>
       ) : (
         <p>{props.reple.reple}</p>
