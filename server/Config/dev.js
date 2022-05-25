7ca912165fc8b7491af6de4acb9e9704bd7f531c
zhak1230 <zhak1230@gmail.com>
2022-05-25 19:52:07 +0900

    HEROKU 사용하여 배표

--- server/Config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURI:
-    'mongodb+srv://zhak1230:dnjsdud639@cluster0.qo0ws.mongodb.net/Community?retryWrites=true&w=majority',
-  access_key: 'Ic00i1OgH0u8wmChTmeb',
-  secret_key: '4K1YSSL11K15bdf7tMuM2MuEB0dYOVRXS33gP5Ra',
-};
