700f570b50e727329fdb70c75a1eef37e3e400de
zhak1230 <zhak1230@gmail.com>
2022-05-31 16:41:34 +0900

    save

--- App/client/src/Component/Post/ImageUpload.js
+++ App/client/src/Component/Post/ImageUpload.js
@@ -4,10 +4,10 @@ import axios from 'axios';
 
 function ImageUpload(props) {
   /*
-    1. 사용자가 이미지를 업로드
-    2. 업로드 한 이미지를 받아서 서버에서 저장
-    3. 저장한 이미지의 경로를 다시 클라이언트에게 전송
-    4. 경로를 받아서 post model에 저장
+    1. 사용자가 이미지를 업로드.
+    2. 업로드 한 이미지를 받아서 서버에서 저장.
+    3. 저장한 이미지의 경로를 다시 클라이언트에게 전송.
+    4. 경로를 받아서 post model에 저장.
     */
 
   const FileUpload = (e) => {
