40eb6bbabebddb560854ac1b9132e8b3205fcdc1
zhak1230 <zhak1230@gmail.com>
2022-05-31 21:09:09 +0900

    이미지 첨부 비동기 처리

--- App/client/src/Component/Post/ImageUpload.js
+++ App/client/src/Component/Post/ImageUpload.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form } from 'react-bootstrap';
 import axios from 'axios';
 
@@ -9,14 +9,26 @@ function ImageUpload(props) {
     3. 저장한 이미지의 경로를 다시 클라이언트에게 전송.
     4. 경로를 받아서 post model에 저장.
     */
+  const [ImageTime, setImageTime] = useState('');
 
-  const FileUpload = (e) => {
+  async function FileUpload(e) {
     var formData = new FormData();
     formData.append('file', e.target.files[0]);
-    axios.post('/api/post/image/upload', formData).then((response) => {
+    try {
+      setImageTime('이미지 첨부중입니다.');
+      const response = await axios.post('/api/post/image/upload', formData);
       props.setImage(response.data.filePath);
-    });
-  };
+      setImageTime('이미지 첨부에 성공하였습니다.');
+    } catch (error) {
+      console.log(error.code);
+    }
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      setImageTime('');
+    }, 3500);
+  }, [ImageTime]);
 
   return (
     <div>
@@ -26,6 +38,7 @@ function ImageUpload(props) {
         accept='image/*'
         onChange={(e) => FileUpload(e)}
       />
+      {ImageTime !== '' && <p>{ImageTime}</p>}
     </div>
   );
 }
